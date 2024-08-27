5c852889cb0548204f784cdd751dbf15fe1ddd1b
vigimus <160164175+vigimus@users.noreply.github.com>
2024-08-27 18:45:14 +0200

    Add files via upload

---
+++ javaScript_Github_Del1.js
@@ -0,0 +1,11 @@
+//Github är ett utmärkt verktyg för att kollaborera och jobba flera samtidigt på olika projekt
+
+//Man kan göra pull-request för att hämta kod man vill åt.
+
+//Även clone med HTTPS för att dela kod med en kollega t.ex https://github.com/vigimus/javaScript_Github_Del1.git
+//Terminal: git --version för att få github versionen
+//cd Downloads
+//git clone https://github.com/vigimus/javaScript_Github_Del1.git
+//OBS! För att kunna använda git kommandon behöver man vara inne på ett .git projekt i terminalen innan
+//git status
+//git add
